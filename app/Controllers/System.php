aa2151039572d7450da7331d42288dca95afb979
DESKTOP-DA580NV\MY <jayant.raikwar03@gmail.com>
2024-07-28 22:02:10 +0530

    add auther name

--- app/Controllers/System.php
+++ app/Controllers/System.php
@@ -5,22 +5,27 @@ namespace App\Controllers;
 use App\Models\TaskModel;
 use App\Models\UserModel;
 
-class System extends BaseController {
-
+class System extends BaseController
+{
+    /**
+     * @Auther JAYANT
+     */
     public $msg;
     protected $columns = ['title', 'description', 'priority', 'start_date', 'due_date'];
     protected $tasksModel;
     protected $userModel;
 
-    function __construct() {
-        
+    function __construct()
+    {
+
         if (!session()->has('user_id')) {
             header('Location: login');
             exit();
         }
         $this->model();
     }
-    function model(){
+    function model()
+    {
         $this->usersModel = new UserModel();
         $this->tasksModel = new TaskModel();
     }
